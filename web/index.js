fd6a9ac2adaab4b17fa1dc0ea0cb93cd70b3e6e1
Anitazc13 <anamaria.zc.13@gmail.com>
2021-11-18 16:38:44 -0500

    Add the method get for the API

--- web/index.js
+++ web/index.js
@@ -1,16 +1,86 @@
 //Dependences to use and libraries
 const http=require('http');
 const express=require('express');
-const app=express();
+const app=express(); // to create routes and api
 const sqlite3=require('sqlite3').verbose();
 const path=require('path');
 
+//settings
+app.set('port',process.env.PORT || 5000)
+
 //Resources
 app.use(express.static(__dirname+'/'))
-
-//Configuration of servidor
+app.use(express.json());
+app.set('json spaces',2);
+//Starting the server
 app.set("view engine","ejs");
-app.set("views", path.join(__dirname,""));
-app.use(express.urlencoded({extended:false}));
+app.set("views", path.join(__dirname,"/"));
+app.use(express.urlencoded({extended:false})); // support the data
 app.listen(5000);
-console.log("Server is running");
+console.log(`Server is running now in the port ${app.get('port')}`);
+
+//Base de Datos
+const db_name=path.join(__dirname,"db","base.db");
+const db=new sqlite3.Database(db_name, err =>{ 
+if (err){
+	return console.error(err.message);
+}else{
+	console.log("Success conexion");
+}
+})
+
+const sql_DROP="DELETE FROM STUDENTS";
+db.run(sql_DROP,err=>{
+	if (err){
+	return console.error(err.message);
+}else{
+	console.log("The table for students was dropped");
+}
+})
+
+//Create the table
+const sql_create="CREATE TABLE IF NOT EXISTS STUDENTS(ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, NAME VARCHAR(100) NOT NULL, SALARY REAL, JOB_NAME VARCHAR(100));";
+db.run(sql_create,err=>{
+	if (err){
+	return console.error(err.message);
+}else{
+	console.log("The table for students was created succesfully");
+}
+})
+
+let students=[
+    {"name": "David", "salary": 2500, "job_name":"Tech Lead"},
+    {"name": "Aldo", "salary": 2300, "job_name":"Back End Developer"},
+    {"name": "Natz", "salary": 1800, "job_name":"Full Stack Developer"},
+    {"name": "Ana", "salary": 1600, "job_name":"Front End Developer"},
+    {"name": "Rodri", "salary": 2150, "job_name":"Front End Developer"},
+    {"name": "Mindy", "salary": 2500, "job_name":"Designer UX/UI"},
+    {"name": "Eder", "salary": 2000, "job_name":"Back End Developer"},
+    {"name": "Erick", "salary": 2100, "job_name":"Full Stack Developer"},
+    {"name": "Andres", "salary": 2500, "job_name":"Tech Lead"},
+    {"name": "Wences", "salary": 5500, "job_name":"Instructor and Mentor"}
+]
+    
+    for (let i=0; i< students.length; i++){
+         db.run('INSERT INTO STUDENTS(Name, Salary, Job_name) VALUES(?, ?, ?)',students[i]["name"],students[i]["salary"],students[i]["job_name"],(err,rows)=>{
+         if(err){
+            throw err;
+         }
+          console.log(`Insert Success of student ${students[i]["name"]}`);
+     })
+    }
+
+//get all the students
+app.get("/",(req,res)=>{
+    const sql= 'SELECT * FROM STUDENTS';
+    let array = []
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+        throw err;
+        }
+        rows.forEach((row) => {
+            array.push(row);
+            });
+            res.json(array);
+    });
+})
